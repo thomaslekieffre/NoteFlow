1d4ede1a0bc3945b6040a490107ce1c28b5c9331
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-10-25 11:20:15 +0200

    fix: clerk

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+const { withClerkMiddleware } = require("@clerk/nextjs/middleware");
+
+module.exports = withClerkMiddleware({
+  // Vos autres configurations Next.js
+});
 
 module.exports = nextConfig;
